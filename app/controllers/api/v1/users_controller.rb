11b873b03727965ea5b8f7b2d9e16e48cd76b073
Thiago Malaquias <tammalaquias@gmail.com>
2022-03-10 13:19:39 -0300

    Adds destroy action to the users controller

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::UsersController < ApplicationController
+  before_action :set_user, only: %i[show update]
+
   def show
     render json: User.find(params[:id])
   end
@@ -12,7 +14,24 @@ class Api::V1::UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @user.destroy
+    head 204
+  end
+
   private
+  
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   # Only allow a trusted parameter "white list" through.
   def user_params
